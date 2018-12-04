7cb7d4269fc8e5b2fd5291c769819362a74efb75
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-12-04 15:02:24 +0530

    Modify test for read function.

--- test/HeadLibTest.js
+++ test/HeadLibTest.js
@@ -2,23 +2,33 @@ const assert = require("assert");
 const {read,
   createDetailsOf} = require("../src/headLib.js");
 
-let readHelloWorld = (file, encoding) => "helloWorld";
-let readEmptyFile = (file, encoding) => "";
+let readHelloWorld = function(file, encoding) {
+  if(file && encoding) {
+    return "helloWorld"
+  }
+};
+
+let readEmptyFile = function(file, encoding) {
+  if(file && encoding) {
+    return "";
+  }
+}
 
 describe("read", function() {
 
-  it("should return the content(\"string\") of provided file", function() {
-    assert(read("../testFile", readHelloWorld, "utf8"), "helloWorld");
-  });
+  it("should return the content of provided file", function() {
+      assert(read("../testFile", readHelloWorld, "utf8"), "helloWorld");
+    });
 
-  it("should return an empty string when an empty file is provided", function() {
-    assert.deepEqual(read("../testFileEmpty", readEmptyFile, "utf8"), "");
+    it("should return an empty string when an empty file is provided", function() {
+      assert.deepEqual(read("../testFileEmpty", readEmptyFile, "utf8"), "");
+    });
   });
-});
 
 describe("createDetailsOf", function() {
 
   it("should return an object with keys \"fileName\" and \"content\" when a file is provided", function() {
+
     let actualOutput = createDetailsOf("../testFile", readHelloWorld, "utf8");
     let expectedOutput = {fileName : "../testFile", content: "helloWorld"};
 
