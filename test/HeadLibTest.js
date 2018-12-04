372118f147d8902761a05fd3832bf4dae1d44e3b
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-12-04 12:02:47 +0530

    Add test for "empty string" for "read function".

--- test/HeadLibTest.js
+++ test/HeadLibTest.js
@@ -2,8 +2,13 @@ const assert = require("assert");
 const {read} = require("../src/headLib.js");
 
 describe("read", function() {
-  let readString = (file,encoding) => file;
-  it("should return idnetical string", function() {
+  let readString = (string, encoding) => string;
+
+  it("should return a string identical to the string provided", function() {
     assert(read("hello world", readString), "hello world");
+  });
+
+  it("should return an empty string when an empty string is provided", function() {
+    assert.deepEqual(read("", readString), "");
   });
 });
