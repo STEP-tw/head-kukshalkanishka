e6057d5c9f5bb1c69eb235b2a1122b0921a70224
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-12-05 17:40:53 +0530

    Refactor validate function.

--- src/errorHandling.js
+++ src/errorHandling.js
@@ -1,5 +1,6 @@
 const validate = function(inputs) {
-  if(inputs.numericOption <= 0 || !isFinite(inputs.numericOption)){
+  let isAlphaMatched = inputs.numericOption != undefined && inputs.numericOption.match(/[a-zA-Z]/);
+  if(inputs.numericOption <= 0 || isAlphaMatched){
     return "head: illegal line count -- " + inputs.numericOption;
   }
 }
