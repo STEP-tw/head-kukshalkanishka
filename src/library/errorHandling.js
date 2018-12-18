d5c3224f45b5ce07d13a4e73485e83ea60497839
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-12-18 16:31:17 +0530

    Refactor 'is option invalid function'.

--- src/library/errorHandling.js
+++ src/library/errorHandling.js
@@ -3,7 +3,7 @@ const isLineOption = option => option == "-n";
 const isCharOption = option => option == "-c";
 
 const isOptionInValid = option =>
-  !isLineOption(option) && !isCharOption(option);
+  !(isLineOption(option) || isCharOption(option));
 
 const isNegativeOrZero = num => num <= 0;
 
@@ -27,7 +27,6 @@ const validateHead = function(params) {
 const validateTail = function(params) {
   const usage =
     "usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]";
-  let choices = { "-n": "line", "-c": "byte" };
   
   if (isOptionInValid(params.option)) {
     return "tail: illegal option -- " + params.option.substr(1) + "\n" + usage;
