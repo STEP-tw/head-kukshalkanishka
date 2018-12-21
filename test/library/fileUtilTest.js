48d4b8167cf5146c30d4e99f024feacfcae8c6b0
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-12-21 17:17:53 +0530

    Add tests for runHead and runTail functions.

--- test/library/fileUtilTest.js
+++ test/library/fileUtilTest.js
@@ -171,3 +171,219 @@ describe("fetchFromBeginning", () => {
     assert.deepEqual(fetchFromBeginning(file1Content, 2), ["t", "h"]);
   });
 });
+
+describe("run tail", function() {
+  describe("runTail basic operation", function() {
+    let file1Content =
+      "this is a line 1\n" +
+      "this is a line 2\n" +
+      "this is a line 3\n" +
+      "this is last line";
+
+    let file2Content = "this is a single line file";
+    let readFileSync = mockReader({ file1: file1Content, file2: file2Content });
+    let existsSync = mockValidator({
+      file1: file1Content,
+      file2: file2Content
+    });
+    let fs = { readFileSync, existsSync };
+
+    it("should return one line content from end of file when the required num of lines is 1", function() {
+      let actual = runTail(
+        { count: "1", option: "-n", filePaths: ["file1"] },
+        fs
+      );
+      let expected = [
+        { filteredContents: "this is last line", filePath: "file1" }
+      ];
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return the whole file when required num of lines is equal to file length", function() {
+      let actual = runTail(
+        { count: "4", option: "-n", filePaths: ["file1"] },
+        fs
+      );
+      let filteredContents =
+        "this is a line 1\n" +
+        "this is a line 2\n" +
+        "this is a line 3\n" +
+        "this is last line";
+      let expected = [{ filePath: "file1", filteredContents }];
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return the whole file when required num of lines is more than file length", function() {
+      let actual = runTail(
+        { count: "10", option: "-n", filePaths: ["file1"] },
+        fs
+      );
+      let filteredContents =
+        "this is a line 1\n" +
+        "this is a line 2\n" +
+        "this is a line 3\n" +
+        "this is last line";
+      let expected = [{ filePath: "file1", filteredContents }];
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return a charactor when required num of chars is 1", function() {
+      let actual = runTail(
+        { option: "-c", count: "1", filePaths: ["file1"] },
+        fs
+      );
+      const expected = [{ filePath: "file1", filteredContents: "e" }];
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return required num of chars from end of the file", function() {
+      let actual = runTail(
+        { option: "-c", count: "4", filePaths: ["file1"] },
+        fs
+      );
+      const expected = [{ filePath: "file1", filteredContents: "line" }];
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return contents and filePaths of all files when multiple files are given", function() {
+      let actual = runTail(
+        { option: "-c", count: "4", filePaths: ["file1", "file2"] },
+        fs
+      );
+      const expected = [
+        { filePath: "file1", filteredContents: "line" },
+        { filePath: "file2", filteredContents: "file" }
+      ];
+
+      assert.deepEqual(actual, expected);
+    });
+  });
+
+  describe("error handling", function() {
+    let readFileSync = mockValidator({ file1: "helloWorld" });
+    let existsSync = mockReader({ file1: "helloWorld" });
+    let fs = { readFileSync, existsSync };
+
+    it("should provide filteredContents as null when invalid file is given", function() {
+      let actual = runTail(
+        { option: "-n", count: "4", filePaths: ["file2"] },
+        fs
+      );
+      const expected = [{ filePath: "file2", filteredContents: null }];
+
+      assert.deepEqual(actual, expected);
+    });
+  });
+});
+
+describe("run head", function() {
+  describe("runead basic operation", function() {
+    let file1Content =
+      "this is a line 1\n" +
+      "this is a line 2\n" +
+      "this is a line 3\n" +
+      "this is last line";
+
+    let file2Content = "this is a single line file";
+    let readFileSync = mockReader({ file1: file1Content, file2: file2Content });
+    let existsSync = mockValidator({
+      file1: file1Content,
+      file2: file2Content
+    });
+    let fs = { readFileSync, existsSync };
+
+    it("should return one line content from end of file when the required num of lines is 1", function() {
+      let actual = runHead(
+        { count: "1", option: "-n", filePaths: ["file1"] },
+        fs
+      );
+      let expected = [
+        { filteredContents: "this is a line 1", filePath: "file1" }
+      ];
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return the whole file when required num of lines is equal to file length", function() {
+      let actual = runHead(
+        { count: "4", option: "-n", filePaths: ["file1"] },
+        fs
+      );
+      let filteredContents =
+        "this is a line 1\n" +
+        "this is a line 2\n" +
+        "this is a line 3\n" +
+        "this is last line";
+      let expected = [{ filePath: "file1", filteredContents }];
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return the whole file when required num of lines is more than file length", function() {
+      let actual = runHead(
+        { count: "10", option: "-n", filePaths: ["file1"] },
+        fs
+      );
+      let filteredContents =
+        "this is a line 1\n" +
+        "this is a line 2\n" +
+        "this is a line 3\n" +
+        "this is last line";
+      let expected = [{ filePath: "file1", filteredContents }];
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return a charactor when required num of chars is 1", function() {
+      let actual = runHead(
+        { option: "-c", count: "1", filePaths: ["file1"] },
+        fs
+      );
+      const expected = [{ filePath: "file1", filteredContents: "t" }];
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return required num of chars from end of the file", function() {
+      let actual = runHead(
+        { option: "-c", count: "4", filePaths: ["file1"] },
+        fs
+      );
+      const expected = [{ filePath: "file1", filteredContents: "this" }];
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return contents and filePaths of all files when multiple files are given", function() {
+      let actual = runHead(
+        { option: "-c", count: "4", filePaths: ["file1", "file2"] },
+        fs
+      );
+      const expected = [
+        { filePath: "file1", filteredContents: "this" },
+        { filePath: "file2", filteredContents: "this" }
+      ];
+
+      assert.deepEqual(actual, expected);
+    });
+  });
+
+  describe("error handling", function() {
+    let readFileSync = mockValidator({ file1: "helloWorld" });
+    let existsSync = mockReader({ file1: "helloWorld" });
+    let fs = { readFileSync, existsSync };
+
+    it("should provide filteredContents as null when invalid file is given", function() {
+      let actual = runHead(
+        { option: "-n", count: "4", filePaths: ["file2"] },
+        fs
+      );
+      const expected = [{ filePath: "file2", filteredContents: null }];
+
+      assert.deepEqual(actual, expected);
+    });
+  });
+});
